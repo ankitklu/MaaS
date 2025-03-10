6a114562fc503095e74bf58a841eb9374346a030
ankitklu <ankit.klu.2022@gmail.com>
2025-03-10 22:32:17 +0530

    Frotnend:- Completed the home page and login page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-        <div className= "h-screen flex justify-between flex-col w-full bg-red-300">
-            <div className="bg-white">
-                <h2>Get started with MaaS</h2>
-                <button>Continue</button>
+        <div className= "bg-cover bg-bottom bg-[url(https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full bg-red-300">
+          <img src="https://mobilityservice.se/resources/logo%20mobility%20service.png" alt="MaaS Logo" className=""/>
+            <div className="bg-white py-5 px-8">
+                <h2 className='text-2xl font-bold text-red-900'>Get started with MaaS</h2>
+                <Link to="/login" className=' bg-[url(https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
             </div>
         </div>
     </div>
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
+
+  const[email, setEmail]= useState('')
+  const[password, setPassword]= useState('') 
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(email + password)
+    setEmail('')  
+    setPassword('')
+  } 
+
+
   return (
-    <div>UserLogin</div>
-  )
+      <div className='p-7 h-screen flex flex-col justify-between'>
+        <div>
+          <img className='w-40 mb-10' src="https://mobilityservice.se/resources/logo%20mobility%20service.png" alt="" />
+  
+          <form onSubmit={(e) => {
+            submitHandler(e)
+          }}>
+            <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+            <input
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+              type="email"
+              placeholder='email@example.com'
+            />
+  
+            <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+  
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+
+              className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+              required type="password"
+              placeholder='password'
+            />
+  
+            <button
+              className='bg-[url(https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+            >Login</button>
+  
+          </form>
+          <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
+        </div>
+        <div>
+          <Link
+            to='/captain-login'
+            className='bg-[url(https://images.unsplash.com/photo-1626139331922-323c61f7f19d?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+          >Sign in as Captain</Link>
+        </div>
+      </div>
+    )
 }
 
 export default UserLogin
