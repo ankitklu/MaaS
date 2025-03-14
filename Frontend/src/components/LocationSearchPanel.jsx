9199070061f39b46f9a54819e58b61e73e60eee6
ankitklu <ankit.klu.2022@gmail.com>
2025-03-14 13:00:00 +0530

    Frontend:- Added the searchpanel and made the cards for the ride.

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 const LocationSearchPanel = () => {
   return (
-    <div>LocationSearchPanel</div>
+    <div>
+      {/* this is just a sample data */}
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-16 p-2 rounded-full'>
+          <i class='ri-map-pin-fill'></i> 
+        </h2>
+       <h4 className='font-medium'>24B, Damanjodi, Sector-3, Nalco Township</h4>
+      </div>
+
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-16 p-2 rounded-full'>
+          <i class='ri-map-pin-fill'></i> 
+        </h2>
+       <h4 className='font-medium'>24B, Damanjodi, Sector-3, Nalco Township</h4>
+      </div>
+
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-16 p-2 rounded-full'>
+          <i class='ri-map-pin-fill'></i> 
+        </h2>
+       <h4 className='font-medium'>24B, Damanjodi, Sector-3, Nalco Township</h4>
+      </div>
+
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-16 p-2 rounded-full'>
+          <i class='ri-map-pin-fill'></i> 
+        </h2>
+       <h4 className='font-medium'>24B, Damanjodi, Sector-3, Nalco Township</h4>
+      </div>
+
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-16 p-2 rounded-full'>
+          <i class='ri-map-pin-fill'></i> 
+        </h2>
+       <h4 className='font-medium'>24B, Damanjodi, Sector-3, Nalco Township</h4>
+      </div>
+      
+    </div>
   )
 }
 
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -46,15 +46,15 @@ const Home = () => {
 
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
         <img className='w-40 mb-10 absolute left-5 top-5' src="https://mobilityservice.se/resources/logo%20mobility%20service.png" alt="" />
-        <div>
+        <div className='w-screen h-screen'>
           {/* Image for temporary use  */}
-          <img className='w-full h-screen object-cover' 
+          <img className='w-screen h-screen object-cover' 
      src="https://images.unsplash.com/photo-1586449480537-3a22cf98b04c" 
      alt="Background" />
         </div>
-        <div className="bg-white flex flex-col justify-end h-screen absolute top-0 w-full p-5">
+        <div className="bg-white flex flex-col justify-end h-screen absolute top-0 w-full">
           <div className='h-[30%] p-6 bg-white relative'>
 
           <h5 ref={panelCloseRef} className='absolute opacity-0 right-6 top-6 text-2xl'>
@@ -88,10 +88,45 @@ const Home = () => {
               className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3' type="text" placeholder='Enter your destination'/>
             </form>
           </div>
-          <div ref={panelRef} className=' bg-red-500 p-5 '> 
+          <div ref={panelRef} className=' bg-white p-5 '> 
               <LocationSearchPanel/>
           </div>
         </div>
+
+        <div className='fixed w-full z-10 bottom-0 bg-white px-3 py-8'>
+          <h3 className='text-2xl font-semibold mb-3'>Choose a Vehicle</h3>
+
+              <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+                <img className='h-14' src="https://purepng.com/public/uploads/large/purepng.com-hyundai-santa-fe-white-carcarvehicletransporthyundai-961524650727bljaq.png" alt="" />
+                <div className='ml-2 w-1/2'>
+                  <h4 className='font-medium text-base'>Uber Go <span><i className="ri-user-3-fill"></i>4</span></h4>
+                  <h5 className='font-medium text-sm'> 2 mins away </h5>
+                  <p className='font-normal text-xs'>Affordable, compact rides</p>
+                </div>
+                <h2 className='text-xl font-semibold'>$193.20</h2>
+              </div>
+
+              <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+                <img className='h-14 w-16' src="https://static.vecteezy.com/system/resources/previews/024/819/250/large_2x/electric-motorbike-electric-bike-e-bike-e-motorbike-electric-vehicle-e-vehicle-transparent-background-ai-generated-png.png" alt="" />
+                <div className='ml-2 w-1/2'>
+                  <h4 className='font-medium text-base'>Uber Bike <span><i className="ri-user-3-fill"></i>1</span></h4>
+                  <h5 className='font-medium text-sm'> 2 mins away </h5>
+                  <p className='font-normal text-xs'>Affordable, bike rides</p>
+                </div>
+                <h2 className='text-xl font-semibold'>$10.20</h2>
+              </div>
+
+              <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+                <img className='h-14' src="https://clipart-library.com/2023/Uber_Auto_312x208_pixels_Mobile.png" alt="" />
+                <div className='ml-2 w-1/2'>
+                  <h4 className='font-medium text-base'>Uber Auto <span><i className="ri-user-3-fill"></i>1</span></h4>
+                  <h5 className='font-medium text-sm'> 2 mins away </h5>
+                  <p className='font-normal text-xs'>Affordable, auto rides</p>
+                </div>
+                <h2 className='text-xl font-semibold'>$200.20</h2>
+              </div>
+        </div>
+
     </div>
   )
 }
