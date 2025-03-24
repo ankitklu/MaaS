b5fee2cca1bf37d708203fce9c3dfbcd82f4b742
ankitklu <ankit.klu.2022@gmail.com>
2025-03-24 22:41:37 +0530

    Frontend 100% done with integrating endpoints on some pages.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -138,12 +138,10 @@ const Home = () => {
   return (
     <div className='h-screen relative overflow-hidden'>
         <img className='w-16 absolute left-5 top-5' src="https://mobilityservice.se/resources/logo%20mobility%20service.png" alt="" />
-        <div className='w-screen h-screen'>
-          {/* Image for temporary use  */}
-          <img className='w-screen h-screen object-cover' 
-     src="https://images.unsplash.com/photo-1586449480537-3a22cf98b04c" 
-     alt="Background" />
-        </div>
+        <div className='h-3/5'>
+                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+
+            </div>
         <div className="bg-white flex flex-col justify-end h-screen absolute top-0 w-full">
           <div className='h-[30%] p-6 bg-white relative'>
 
